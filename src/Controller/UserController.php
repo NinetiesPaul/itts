d724f14f682442c9333847a792d61271f6fa862d
Paul Richard <paulrpm@gmail.com>
2024-01-28 20:12:10 -0300

    reverted previous commit

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,15 +3,34 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
-    #[Route('/user/home', name: 'user_home')]
+    #[Route('/user/home', name: 'user_home', methods: ['GET'])]
     public function index(): Response
     {
         return $this->render('users/home.html.twig', [
+        ]);
+    }
+
+    #[Route('/user/call/new', name: 'user_create_call_form', methods: ['GET'])]
+    public function createCallForm(): Response
+    {
+        return $this->render('users/create_call.html.twig', [
+        ]);
+    }
+
+    #[Route('/user/call', name: 'user_create_call', methods: ['POST'])]
+    public function createCall(): JsonResponse
+    {
+        return $this->json([
+            'success' => true,
+            'data' => [
+                'message' => "Call placed"
+            ]
         ]);
     }
 }
