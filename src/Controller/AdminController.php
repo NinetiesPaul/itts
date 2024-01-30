cd2d6675a551158cc5ebd0de88e45e3c8026ee64
Paul Richard <paulrpm@gmail.com>
2024-01-30 09:33:39 -0300

    added routes to take and close call

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\CallNotes;
 use App\Entity\Calls;
+use App\Entity\Users;
+use DateTime;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -44,6 +49,73 @@ class AdminController extends AbstractController
         
         return $this->render('admin/call_view.html.twig', [
             'call' => $call,
+        ]);
+    }
+
+    #[Route('/admin/call/take', name: 'admin_take_call', methods: ['POST'])]
+    public function takeCall(ManagerRegistry $doctrine, Request $request): JsonResponse
+    {
+        $entityManager = $doctrine->getManager();
+
+        $call = $doctrine->getRepository(Calls::class)->find($request->get('id'));
+
+        $message = "Call answered successfully";
+        $success = true;
+        if (!$call->getAnsweredBy()) {
+            $user = $doctrine->getRepository(Users::class)->find($this->getUser()->getId());
+            $now = new DateTime();
+
+            $call_note = new CallNotes();
+            $call_note->setText("Hi! I'll be taking your call! I'm on my way to you now!");
+            $call_note->setCallId($call);
+            $call_note->setSentBy($user);
+            $call_note->setSentOn($now);
+            $entityManager->persist($call_note);
+
+            $call->setAnsweredBy($user);
+            $call->setAnsweredOn($now);
+            $entityManager->flush();
+        } else {
+            $message = "Call answer failed: already taken by someone";
+            $success = false;
+        }
+
+        return $this->json([
+            'success' => $success,
+            'data' => [
+                'message' => $message,
+                'payload' => []
+            ]
+        ]);
+    }
+
+    #[Route('/admin/call/close', name: 'admin_close_call', methods: ['POST'])]
+    public function closeCall(ManagerRegistry $doctrine, Request $request): JsonResponse
+    {
+        $entityManager = $doctrine->getManager();
+
+        $call = $doctrine->getRepository(Calls::class)->find($request->get('id'));
+
+        $user = $doctrine->getRepository(Users::class)->find($this->getUser()->getId());
+        $now = new DateTime();
+
+        $call_note = new CallNotes();
+        $call_note->setText("Hi! It seems that this issue has been deemed as resolved by the technician, so it has been closed! Thank you!");
+        $call_note->setCallId($call);
+        $call_note->setSentBy($user);
+        $call_note->setSentOn($now);
+        $entityManager->persist($call_note);
+
+        $call->setClosedBy($user);
+        $call->setClosedOn($now);
+        $entityManager->flush();
+
+        return $this->json([
+            'success' => true,
+            'data' => [
+                'message' => 'Call closed successfully',
+                'payload' => []
+            ]
         ]);
     }
 }
